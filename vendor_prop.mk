8880fa1f6604103b32a14565955f39ed7b232071
Roker2 <dimaminko2000@gmail.com>
2019-11-24 20:39:40 +0000

    mido: Fix lags on some Google wallpapers
    Change-Id: I6120ff7de20f474786c3a4bca208159fe812e9ee

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -70,7 +70,7 @@ debug.gralloc.enable_fb_ubwc=1 \
 debug.mdpcomp.logs=0 \
 debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
-debug.sf.latch_unsignaled=1 \
+debug.sf.latch_unsignaled=0 \
 debug.sf.recomputecrop=0 \
 debug.sf.disable_backpressure=1 \
 dev.pm.dyn_samplingrate=1 \
@@ -186,6 +186,10 @@ persist.vendor.delta_time.enable=true
 # Tcp
 PRODUCT_PROPERTY_OVERRIDES += \
 net.tcp.2g_init_rwnd=10
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+sys.use_fifo_ui=0
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
