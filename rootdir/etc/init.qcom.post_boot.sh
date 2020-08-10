a92c98a256e59913293f97310d29071c3e194628
Khalakuzzaman Apon <khalakuzzamanapon5@gmail.com>
2020-08-10 16:37:20 +0000

    mido: rootdir: hispeed to 400457

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -35,7 +35,7 @@ function 8953_sched_dcvs_eas()
     echo 20000 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
     echo 500 > /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
     #set the hispeed_freq
-    echo 652800 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_freq
+    echo 400457 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_freq
     #default value for hispeed_load is 90, for 8953 and sdm450 it should be 85
     echo 85 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_load
 }
