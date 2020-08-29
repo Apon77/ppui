a6d401654b28af723f9d29f0db4b7f1bfc7c9a9b
Khalakuzzaman Apon <khalakuzzamanapon5@gmail.com>
2020-08-30 02:53:36 +0600

    mido: Xiaomiparts: Spectrum: Set default profile to 0
    Copied from Ethereal kernel mido. Thanks SJD Ayy

--- rootdir/etc/init.spectrum.sh
+++ rootdir/etc/init.spectrum.sh
@@ -1,7 +1,13 @@
+#!/system/bin/sh
 # SPECTRUM KERNEL MANAGER
 # Profile initialization script by nathanchance
 
 # If there is not a persist value, we need to set one
 if [ ! -f /data/property/persist.spectrum.profile ]; then
+    setprop persist.spectrum.profile 0
+fi
+
+#Profile initialization script copied from Ethereal kernel
+if [ "`getprop persist.spectrum.profile`" == "" ]; then
     setprop persist.spectrum.profile 0
 fi
