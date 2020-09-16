635ea86e85c062c6c8b12c88cdbabfc962fe8d46
GiaSen <giammasena@gmail.com>
2020-09-16 19:22:36 +0200

    mido: Nuke libinit [WIP]

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,10 +155,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_mido
-TARGET_RECOVERY_DEVICE_MODULES := libinit_mido
-
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
