d0c7487fe730c1e7b24a4e8ee35bff4f5c6eb01c
Apon77 <khalakuzzamanapon5@gmail.com>
2020-10-20 16:29:23 +0600

    mido: maintainer

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -42,7 +42,9 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # PixelPlusUI Maintainer
-PPUI_MAINTAINER := KhalakuzzamanApon
+#PPUI_MAINTAINER := KhalakuzzamanApon
+PRODUCT_PROPERTY_OVERRIDES += \
+      ro.pixelplusui.maintainer=Khalakuzzaman Apon
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
