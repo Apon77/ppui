6fca2b07402d9fafc31381ac8bfc4cb9cfe38ebf
Apon77 <khalakuzzamanapon5@gmail.com>
2020-11-24 22:30:44 +0600

    Revert "AEXify partial"
    give apn, messaging app back plox
    
    This reverts commit 916e5de931d4d90f801b52ec491c02d983d4e80a.

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common AEX stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
