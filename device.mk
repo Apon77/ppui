345c278e7995117dbc26a18c6a07d443c9e85385
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-12-10 17:35:34 -0600

    mido: Build Open Camera instead of Snap Camera
    This because Snap Camera has broken video recording while opencamera not
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -145,7 +145,10 @@ PRODUCT_PACKAGES += \
     camera.msm8953 \
     libmm-qcamera \
     libui_shim \
-    Snap
+    OpenCamera
+
+RODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/net.sourceforge.opencamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/net.sourceforge.opencamera.xml
 
 # Component overrides
 PRODUCT_COPY_FILES += \
