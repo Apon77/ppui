cdd131f38e54ea553c393ecd53995f4b976025ae
Apon77 <khalakuzzamanapon5@gmail.com>
2021-01-06 23:38:39 +0600

    Revert "mido: Import XiaomiParts v3.0"
    This reverts commit 00de90d62b9c3e8eb73581f6e876e67b63d3350d.

--- device.mk
+++ device.mk
@@ -416,10 +416,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# XiaomiParts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # SimpleKeyboard
 PRODUCT_PACKAGES += \
     SimpleKeyboard
