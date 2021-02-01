c5c11437766e934bdf0f6ac15abc4651d855d1c3
Apon77 <khalakuzzamanapon5@gmail.com>
2021-02-01 15:23:39 +0600

    Revert "mido: xiaomiparts: ignore neverallow"
    This reverts commit dedf90122ff2bffc3f40e3207c7a0678d0311fce.

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-export SELINUX_IGNORE_NEVERALLOWS=true
-
-#For aex
-cd system/sepolicy && git fetch "http://gerrit.aospextended.com/AospExtended/platform_system_sepolicy" refs/changes/77/4677/1 && git cherry-pick FETCH_HEAD && cd ../..
