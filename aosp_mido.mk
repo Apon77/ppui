bfa662657b7da3326139228e8a39db8bb52b72cc
Apon77 <khalakuzzamanapon5@gmail.com>
2021-03-24 17:20:34 +0600

    fix device name

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -32,7 +32,7 @@ TARGET_FACE_UNLOCK_SUPPORTED = true
 CUSTOM_BUILD_TYPE=OFFICIAL
 
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.ppui.device_name=Redmi Note 4x \
+ro.ppui.device_name="Redmi Note 4x" \
 ro.ppui.version=3.3 \
 ro.ppui.version_code=Tanzanite \
 ro.ppui.is_official=true \
